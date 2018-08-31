88ad694a157205393485222d0fffd06e9f97b583
TheGreyHawk01 <kshimasaki3811@myhta.org>
2018-08-30 14:19:01 -1000

    finished

---
+++ app.js
@@ -0,0 +1,47 @@
+
+window.onload=function(){
+    var funkyFunctions = {
+        salsa: function(){
+            var danceMoves = [3, 2, 1, 5, 6];
+            var numSteps = 0;
+            for (var i = 0; i < danceMoves.length; i++){
+                numSteps += danceMoves[i];
+            }
+            return numSteps;
+        },
+        chickenDance: function(){
+            //a method is a function inside an object
+        var numClucks = 0;
+        for(var i = 0; i < 5; i++){
+            numClucks = numClucks + 5;
+        }
+        var chicken = {
+
+            funky1: function(){
+                numCluck = numCluck + 10;
+                return numCluck;
+            },
+            funky2: function(){
+            return (chicken.funky1());
+            }
+        }
+
+        },
+    shuffle: function(){
+var type = ['melbourne', 'electronic', 'hardstyle'];
+var myShuffle = {type: type[1]; isPopular: true; numClasp: 300}
+if(var i = 0; i == myShuffle; i++){
+    return ()
+}
+}
+
+
+        
+
+
+
+
+        
+        }
+    }
+}
